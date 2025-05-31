11b43bf6c8154247845a56c48ac13dab72a6a7ed
RKEX <151117052+RKEX@users.noreply.github.com>
2025-05-31 21:27:24 +0530

    Update SeoComp.tsx

--- src/components/SeoComp/SeoComp.tsx
+++ src/components/SeoComp/SeoComp.tsx
@@ -3,7 +3,7 @@ import SeoSearchCom from "./SeoSearchCom";
 const SeoComp = () => {
   return (
     <>
-      <div className="bg-orange-300/25 w-full flex justify-center py-6">
+      <div className="bg-orange-300/25 flex justify-center py-6">
         <div className="grid gap-2">
           {/* text 1 start here */}
           <div className="font-bold font-sans text-center mt-4 text-2xl">
